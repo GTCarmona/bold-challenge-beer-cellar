21208ca3225de28485313d56f179d6cc7f57c349
Guilherme Carmona <guilherme.carmona@outlook.com>
2019-11-11 19:31:09 +0000

    card deck in beer views added

--- client/src/components/pages/Beers.jsx
+++ client/src/components/pages/Beers.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react"
 import api from "../../api"
+import Card from "react-bootstrap/Card"
+import CardDeck from "react-bootstrap/CardDeck"
 
 export default function Beers() {
   const [beers, setBeers] = useState([])
@@ -15,9 +17,23 @@ export default function Beers() {
   return (
     <div className="Beers">
       <h2>List of Beers</h2>
-      {beers.map(beer => (
-        <li key={beer._id}>{beer.name}</li>
-      ))}
+      <CardDeck>
+        {beers.map(beer => (
+          <Card key={beer._id}>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>{beer.name}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Type: {beer.type}
+                <br />
+                Style: {beer.style}
+              </Card.Subtitle>
+
+              <Card.Text>{beer.description}</Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
+      </CardDeck>
     </div>
   )
 }
